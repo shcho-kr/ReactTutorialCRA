94b1621a9093053411076d04b8e6da315af56a15
kyushu1996@gmail.com <103431041+shcho-kr@users.noreply.github.com>
2023-03-25 02:17:05 +0900

    cleanup func

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Button from './Button';
 import styles from './App.module.css';
 import { useState, useEffect } from 'react';
-
+import { func } from 'prop-types';
+/* 
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
@@ -19,6 +20,34 @@ function App() {
       <input value={keyword} onChange={onChange} type="text" placeholder='Search here...' />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
+    </div>
+  );
+}
+ */
+
+function Hello() {
+  useEffect(() => {
+    console.log("created!");
+    return () => console.log("destroyed");
+  }, []);
+  // function byeFn() {
+  //   console.log("bye");
+  // }
+  // function hiFn() {
+  //   console.log("hi");
+  //   return byeFn;
+  // }
+  // useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
