5de7ddb85f45b1271a61b295fe0b40d1a4b94fbb
kyushu1996@gmail.com <103431041+shcho-kr@users.noreply.github.com>
2023-03-24 23:41:25 +0900

    effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Button from './Button';
 import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("all time");
+  useEffect(() => {
+    console.log("Call the api");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "") console.log("Search "+ keyword);
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <input value={keyword} onChange={onChange} type="text" placeholder='Search here...' />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
